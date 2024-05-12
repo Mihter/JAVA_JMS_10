c936c950b05e82171b3accde62cf4b8bd997231f
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2024-05-12 16:12:49 +0500

    start

---
+++ src/main/java/com/example/java_jms_10/JavaJms10Application.java
@@ -0,0 +1,13 @@
+package com.example.java_jms_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJms10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJms10Application.class, args);
+	}
+
+}
